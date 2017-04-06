ece078de944d771363d2e9474585de4b217ebd0b
Pedro Sfriso <p.sfriso@LAP1145.local>
2017-04-06 13:09:07 +0100

    added flag for cluster

--- config.mk
+++ config.mk
@@ -1,5 +1,6 @@
 #DEBUG = -g -fbacktrace -fbounds-check 
 DEBUG = 
+DEBUGNOCHECK = 
 PROFILE = -pg
 OPTIONS = -O3 -fimplicit-none -funroll-loops -mtune=generic -Wall -ftree-vectorizer-verbose=1 -ffast-math  
 LOPTIONS= -static-libgfortran  
@@ -8,5 +9,6 @@ CPP= cpp
 LIBDIR = ../lib
 CFLAGS = -c $(DEBUG) $(PROFILE) -I$(LIBDIR)
 CFLAGSNODEBUG = -c $(PROFILE) -I$(LIBDIR)
+CFLAGSNOCHECK = -c $(DEBUGNOCHECK) $(PROFILE) -I$(LIBDIR)
 LFLAGS = $(PROFILE) -L$(LIBDIR) 
 ENDFLAG = -fconvert=little-endian
